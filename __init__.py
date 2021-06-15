6e05b440fed66bc9ddfc3535f7223de67e056743
Tobia <tobiadig@gmail.com>
2021-06-15 09:21:01 +0200

    platePy

---
+++ __init__.py
@@ -0,0 +1,18 @@
+"""
+platePy
+=====
+
+Provides\n
+* Methods to easily model structural components for plates via the gmsh API (see https://gmsh.info/). \n
+* Mesh generation via gmsh API. \n
+* FEM calculation of the system. \n
+* Results visualization. \n
+* Analythical solutions according to Kirchhoff plate theory.
+"""
+
+from .analyticPlateSolutions import *
+from .createModel import *
+from .generateMesh import *
+from .solveModel import *
+from .displayModel import *
+
