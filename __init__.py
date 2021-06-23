a33df5e6daa488f94264f6ad0539d061b43e4b75
tobiadig <81258612+tobiadig@users.noreply.github.com>
2021-06-23 10:49:50 +0200

    push to github

--- __init__.py
+++ __init__.py
@@ -3,11 +3,11 @@ Provides
 
 * Methods to easily model structural components for plates via the gmsh API (see https://gmsh.info/).
 * Mesh generation via gmsh API.
-* FEM calculation of the system.
+* FEM calculation of displacements, rotations, bending moments, shear forces and required bending moment resistance according to SIA262.
 * Results visualization.
 * Analythical solutions according to Kirchhoff plate theory.
 
-Copywrite Tobia Diggelmann (ETH Zurich) 23.06.2021.
+*Copywrite Tobia Diggelmann (ETH Zurich) 23.06.2021.*
 '''
 
 from .analyticPlateSolutions import *
