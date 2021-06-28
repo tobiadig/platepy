12cc9341df1ed633b2a7edcc8ddbd24c471226c6
tobiadig <81258612+tobiadig@users.noreply.github.com>
2021-06-28 18:01:13 +0200

    tut4

--- __init__.py
+++ __init__.py
@@ -159,7 +159,7 @@ The created images can be manually saved through the plt.show() interface, or au
 
     # ------------------------------------------------------------------------------
     #
-    #  platepy tutorial 1
+    # platepy tutorial 1
     #
     # Creating a simple slab geometry and computing the FEM solution
     #
@@ -259,7 +259,7 @@ The created images can be manually saved through the plt.show() interface, or au
 
     # ------------------------------------------------------------------------------
     #
-    #  platepy tutorial 2
+    # platepy tutorial 2
     #
     # Downstand beams and plotting results over a line ('Schnitte')
     #
@@ -331,14 +331,14 @@ The created images can be manually saved through the plt.show() interface, or au
 
     # ------------------------------------------------------------------------------
     #
-    #  platepy tutorial 3
+    # platepy tutorial 3
     #
     # Manually create a simple mesh
     #
     # ------------------------------------------------------------------------------
 
 
-    #The present tutorial shows how to manually define nodes, element, boundary conditions
+    # The present tutorial shows how to manually define nodes, element, boundary conditions
     # and nodal loads. This can be useful when a simple, yet precise defined mesh is required
     # For exampl for a patch test.
 
@@ -411,6 +411,60 @@ The created images can be manually saved through the plt.show() interface, or au
     plotResults(tutorialModel,plotType='text+mesh',valuesToPlotList=['vDisp'])
     plt.show()
 
+<a name="t4"></a>A.4. Compute analytical solutions
+
+    # ------------------------------------------------------------------------------
+    #
+    # platepy tutorial 4
+    #
+    # Compute analytical solution according to Timoshenko's formulas
+    #
+    # ------------------------------------------------------------------------------
+
+    # This tutorial shows how to use the analyticPlateSolutions module
+    # We will access the displacement at the location (x=-2, y=1) (relative 
+    # to the plate's center) of a simply supported rectangular plate with shape
+    # a=10, b=12
+
+    from platepy import *
+
+    # First, let's define the input options as attributes of the POpts class
+    # The possible entries are described in the documentation
+    pOpts = POpts()
+    pOpts.shape="rectangular"
+    pOpts.depth = "thin"
+    pOpts.support = "simplySupported"
+    pOpts.geometry = (10,12)  # width a and high b
+    pOpts.material = Material(10920, 0.3, 0.1) #E, nu and thickness
+
+    # Here the Load options are defined, as attributes of the LOpts class
+    # The possible entries are described in the documentation
+    lOpts = LOpts()
+    lOpts.type = "distributed"
+    lOpts.magnitude = 1
+
+    # The number of term for the Taylor expantion has to be defined.
+    # 40 terms is usually more than enough
+    sOpts = SOpts()
+    sOpts.nTerms = 40
+
+    #  inPos is a nPointsx2 numpy array containing the x-y coordinates of
+    # the points we want to evaluate. In our case we only need on point.
+    xPos = -2
+    yPos = 1
+    inPos=np.array([[xPos,yPos]])  # x-y coordinates
+
+    quantities, values, outPos = AnalyticPlateSolutions(pOpts, lOpts, sOpts, inPos)
+
+    # values is a (nPoints, nOutputs) array of the quantities values evaluated at outPos.
+    # the output values correspond to the True entries of the array quantities.
+    # the entries of quantities correspond to: (vDisp, Mx, My, Mxy, Vx, Vy).
+    # In this case, all values are available apart from the twisting moment Mxy, therefore
+    # values has shape (1,5).
+
+    vDisp_mm = values[0,0]*1000
+
+    print("The displacement at (", xPos,",",yPos,") corresponds to ", vDisp_mm, ' mm')
 
 
 '''
